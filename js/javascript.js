47ea6dd9dd5547a6d6ce25ea5d2ac852271b9277
Lucas Betinardi <lbs@dr.com>
2014-11-01 05:38:00 -0500

    jquery rewritten, added sliding effect while hiding and showing the sign-up part of the form, added verification of the form based on what radio option was selected

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
-    $('input[type="radio"]').click(function(){
-        if($(this).attr("value")=="yes"){
+    $('.user-password').click(function(){
+        if($('#new-user').is(':checked')){
             $(".user-signin").show();
-            $(".user-signup").hide();
-            $("#login").attr("action","/signin");
-        }
-        if($(this).attr("value")=="no"){
-            $(".user-signin").show();
-            $(".user-signup").show();
+            $(".user-signup").slideDown();
             $("#login").attr("action","/signup");
+            $('#first-name').prop('required', true);
+            $('#last-name').prop('required', true);
+            $('#term').prop('required', true);
+        } 
+        if($('#exist-user').is(':checked')){
+            $(".user-signin").show();
+            $(".user-signup").slideUp();
+            $("#login").attr("action","/signin");
+            $('#first-name').prop('required', false);
+            $('#last-name').prop('required', false);
+            $('#term').prop('required', false);
         }
     });
 });
